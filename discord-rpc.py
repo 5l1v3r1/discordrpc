4c5338853a6e8351515a9bbe07fef3eeb9c8621a
doop <75194878+7uk@users.noreply.github.com>
2021-06-30 02:27:02 +0100

    Update discord-rpc.py

--- discord-rpc.py
+++ discord-rpc.py
@@ -1,1 +1,95 @@
+import time, pypresence, struct, os, colorama
 
+from pypresence import *
+from colorama import Fore
+
+os.system('cls')
+
+print(f"""
+{Fore.BLUE}██████╗ ██╗ ██████╗ █████╗  █████╗ ██████╗ ██████╗       ██████╗ ██████╗  █████╗
+██╔══██╗██║██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗      ██╔══██╗██╔══██╗██╔══██╗{Fore.CYAN}
+██║  ██║██║╚█████╗ ██║  ╚═╝██║  ██║██████╔╝██║  ██║█████╗██████╔╝██████╔╝██║  ╚═╝
+██║  ██║██║ ╚═══██╗██║  ██╗██║  ██║██╔══██╗██║  ██║╚════╝██╔══██╗██╔═══╝ ██║  ██╗{Fore.RESET}
+██████╔╝██║██████╔╝╚█████╔╝╚█████╔╝██║  ██║██████╔╝      ██║  ██║██║     ╚█████╔╝
+╚═════╝ ╚═╝╚═════╝  ╚════╝  ╚════╝ ╚═╝  ╚═╝╚═════╝       ╚═╝  ╚═╝╚═╝      ╚════╝ 
+""")
+
+clientidinput = f'[{Fore.BLUE}+{Fore.RESET}] Please enter your applications Client ID: '
+rpcstatusinput = f'[{Fore.BLUE}+{Fore.RESET}] Please enter your applications Status: '
+rpcdetailsinput = f'[{Fore.BLUE}+{Fore.RESET}] Please enter your applications Details: '
+refreshtimeinput = f'[{Fore.BLUE}+{Fore.RESET}] Please enter your applications Refresh Time: '
+
+largeiconname_input = f'[{Fore.BLUE}+{Fore.RESET}] Please enter your applications Large Icons Name: '
+largeicontext_input = f'[{Fore.BLUE}+{Fore.RESET}] Please enter your Large Icons Text: '
+smalliconname_input = f'[{Fore.BLUE}+{Fore.RESET}] Please enter your applications Small Icons Name: '
+smallicontext_input = f'[{Fore.BLUE}+{Fore.RESET}] Please enter your Small Icons Text: '
+
+button1text_input = f'[{Fore.BLUE}+{Fore.RESET}] Please enter your 1st Buttons Name: '
+button1link_input = f'[{Fore.BLUE}+{Fore.RESET}] Please enter your 1st Buttons Link: '
+button2text_input = f'[{Fore.BLUE}+{Fore.RESET}] Please enter your 2nd Buttons Name: '
+button2link_input = f'[{Fore.BLUE}+{Fore.RESET}] Please enter your 2nd Buttons Link: '
+
+
+clientID = input(f'{clientidinput}')
+print(' ')
+rpcstatus = input(f'{rpcstatusinput}')
+print(' ')
+rpcdetails = input(f'{rpcdetailsinput}')
+print(' ')
+refreshtime = input(f'{refreshtimeinput}')
+print(' ')
+largeiconname = input(f'{largeiconname_input}')
+print(' ')
+largeicontext = input(f'{largeicontext_input}')
+print(' ')
+smalliconname = input(f'{smalliconname_input}')
+print(' ')
+smallicontext = input(f'{smallicontext_input}')
+print(' ')
+button1text = input(f'{button1text_input}')
+print(' ')
+button1link = input(f'{button1link_input}')
+print(' ')
+button2text = input(f'{button2text_input}')
+print(' ')
+button2link = input(f'{button2link_input}')
+print(' ')
+
+os.system('cls')
+
+print(f"""
+{Fore.BLUE}██████╗ ██╗ ██████╗ █████╗  █████╗ ██████╗ ██████╗       ██████╗ ██████╗  █████╗
+██╔══██╗██║██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗      ██╔══██╗██╔══██╗██╔══██╗{Fore.CYAN}
+██║  ██║██║╚█████╗ ██║  ╚═╝██║  ██║██████╔╝██║  ██║█████╗██████╔╝██████╔╝██║  ╚═╝
+██║  ██║██║ ╚═══██╗██║  ██╗██║  ██║██╔══██╗██║  ██║╚════╝██╔══██╗██╔═══╝ ██║  ██╗{Fore.RESET}
+██████╔╝██║██████╔╝╚█████╔╝╚█████╔╝██║  ██║██████╔╝      ██║  ██║██║     ╚█████╔╝
+╚═════╝ ╚═╝╚═════╝  ╚════╝  ╚════╝ ╚═╝  ╚═╝╚═════╝       ╚═╝  ╚═╝╚═╝      ╚════╝ 
+""")
+
+print(f'[{Fore.BLUE}+{Fore.RESET}] Discord-RPC Started Successfully...')
+
+RPC = Presence(clientID)
+RPC.connect()
+starttime = int(time.time())
+
+try:
+    while True:
+        RPC.update(
+            state = rpcdetails,
+            details = rpcstatus,
+            large_image = largeiconname,
+            large_text = largeicontext,
+            small_image = smalliconname,
+            small_text = smallicontext,
+            start = starttime,
+            buttons = [{"label": button1text, "url": button1link}, {"label": button2text, "url": button2link}]
+        )
+        time.sleep(int(refreshtime))
+except pypresence.exceptions.InvalidPipe as error1:
+    print(f"\n[{Fore.RED}!{Fore.RESET}] Make sure Discord is Running!")
+except pypresence.exceptions.InvalidID  as error2:
+    print(f"\n[{Fore.RED}!{Fore.RESET}] Invalid Client ID!")
+except struct.error as error3:
+    print(f"\n[{Fore.RED}!{Fore.RESET}] Invalid Client ID!")
+except pypresence.exceptions.ServerError as error4:
+    print(f"\n[{Fore.RED}!{Fore.RESET}] Please make sure you entered the correct Link/Name/ID.")
